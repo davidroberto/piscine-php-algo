02d775c43e858c28790cf06d76d98af1e1ecf78b
David Robert <contact@david-robert.fr>
2025-03-10 14:00:23 +0100

    echo in html

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 	// je créé une variable "name" dont la valeur est david
 	$name = "david";
-
 ?>
 
-<h1>Bonjour David</h1>
+
+
+<!-- je demande au php de générer du html avec un H1, incluant Bonjour, suivi de la valeur de la variable
+ $name, suivi de la fermeture de la balise H1. Pour faire une instruction php dans du html, je reouvre la balise php.
+echo en php permet d'afficher une valeur.
+  -->
+<h1>Bonjour <?php echo $name; ?> </h1>
