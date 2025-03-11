efeb37d5d7f066db3865503588c85f0105e1eba6
David Robert <contact@david-robert.fr>
2025-03-11 17:30:27 +0100

    return in functions

--- calculator.php
+++ calculator.php
@@ -1,15 +1,38 @@
 <?php 
 
-// la fonction n'affiche plus le résultat
-// elle fait juste l'addition et retourne le résultat
 function addNumbers($number1, $number2) {
 	return $number1 + $number2;
 }
 
-// je récupère la valeur de retour de la fonction
-// et je la stocke dans une variable
-$result = addNumbers(20, 20);
+function substractNumbers($number1, $number2) {
+	return $number1 - $number2;
+}
+
+function divibeBy2($number1) {
+	return $number1 / 2;
+}
+
+
+function calculateSquareRoot($number1) {
+	return sqrt($number1);
+}
 
 ?>
 
-<h1><?php echo $result; ?></h1>
+<html>
+
+	<?php $resultAdd = addNumbers(10, 20); ?>
+	<h1>Le résultat de l'addition est : <?php echo $resultSubstract; ?></h1>
+
+	<?php $resultSubstract = substractNumbers(10, 5); ?>
+	<h1>Le résultat de la soustraction est : <?php echo $resultSubstract; ?></h1>
+
+
+	<?php $resultDivide = divibeBy2(34); ?>
+	<h1>Le résultat de la division est : <?php echo $resultDivide; ?></h1>
+
+	<?php $resultSquareRoot = calculateSquareRoot(25); ?>
+	<h1>Le résultat de la racine carrée est : <?php echo $resultSquareRoot; ?></h1>
+
+
+</html>
