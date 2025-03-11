cae60c743352c4a705700099558f65ae6cccb6c0
David Robert <contact@david-robert.fr>
2025-03-11 15:15:56 +0100

    message in function

--- calculator.php
+++ calculator.php
@@ -8,21 +8,21 @@ addNumbers(10, 20);
 
 
 function substractNumbers($number1, $number2) {
-	echo "<p>le résultat de la soustraction est : " . $number1 - $number2 . "</p>";
+	echo "<p>le résultat de la soustraction de " . $number1 . " et " . $number2 . " est : " . $number1 - $number2 . "</p>";
 }
 
 substractNumbers(10, 5);
 
 
 function divibeBy2($number1) {
-	echo "<p>le résultat de la division par deux est : " . $number1 / 2 . "</p>";
+	echo "<p>le résultat de la division de ". $number1 ." par deux est : " . $number1 / 2 . "</p>";
 }
 
 divibeBy2(34);
 
 
 function calculateSquareRoot($number1) {
-	echo "<p>le résultat de la racine carrée est : " . sqrt($number1) . "</p>";
+	echo "<p>le résultat de la racine carrée de " . $number1 . " est : " . sqrt($number1) . "</p>";
 }
 
 calculateSquareRoot(25);
