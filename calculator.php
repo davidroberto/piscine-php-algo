0c7852bd8572a3ba509ef328d85e37386bc520de
David Robert <contact@david-robert.fr>
2025-03-11 14:41:27 +0100

    function arithmetiques

---
+++ calculator.php
@@ -0,0 +1,30 @@
+<?php 
+
+function addNumbers($number1, $number2) {
+	echo "<p>le résultat de l'addition de " . $number1 . " et " . $number2 . " est : " . $number1 + $number2 . "</p>";
+}
+
+addNumbers(10, 20);
+
+
+function substractNumbers($number1, $number2) {
+	echo "<p>le résultat de la soustraction est : " . $number1 - $number2 . "</p>";
+}
+
+substractNumbers(10, 5);
+
+
+function divibeBy2($number1) {
+	echo "<p>le résultat de la division par deux est : " . $number1 / 2 . "</p>";
+}
+
+divibeBy2(34);
+
+
+function calculateSquareRoot($number1) {
+	echo "<p>le résultat de la racine carrée est : " . sqrt($number1) . "</p>";
+}
+
+calculateSquareRoot(25);
+
+?>
