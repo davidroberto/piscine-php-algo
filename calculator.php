b9ebb611a6da9082eb254329f9edf51d03063d4e
David Robert <contact@david-robert.fr>
2025-03-11 16:42:19 +0100

    return value in addNumber function

--- calculator.php
+++ calculator.php
@@ -1,30 +1,15 @@
 <?php 
 
+// la fonction n'affiche plus le résultat
+// elle fait juste l'addition et retourne le résultat
 function addNumbers($number1, $number2) {
-	echo "<p>le résultat de l'addition de " . $number1 . " et " . $number2 . " est : " . $number1 + $number2 . "</p>";
+	return $number1 + $number2;
 }
 
-addNumbers(10, 20);
-
-
-function substractNumbers($number1, $number2) {
-	echo "<p>le résultat de la soustraction de " . $number1 . " et " . $number2 . " est : " . $number1 - $number2 . "</p>";
-}
-
-substractNumbers(10, 5);
-
-
-function divibeBy2($number1) {
-	echo "<p>le résultat de la division de ". $number1 ." par deux est : " . $number1 / 2 . "</p>";
-}
-
-divibeBy2(34);
-
-
-function calculateSquareRoot($number1) {
-	echo "<p>le résultat de la racine carrée de " . $number1 . " est : " . sqrt($number1) . "</p>";
-}
-
-calculateSquareRoot(25);
+// je récupère la valeur de retour de la fonction
+// et je la stocke dans une variable
+$result = addNumbers(20, 20);
 
 ?>
+
+<h1><?php echo $result; ?></h1>
--- index.php
+++ index.php
@@ -47,8 +47,6 @@ function cutText($textToCut) {
 	
 }
 
-
-
 ?>
 
 
